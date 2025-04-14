07400dbe43cf2a5fdace7cb4d7cbc939cdd3d6b3
alex <alexadkl@gmail.com>
2025-04-14 17:01:41 +0200

    ajout likes

--- frontBlog/src/App.vue
+++ frontBlog/src/App.vue
@@ -1,4 +1,16 @@
-<script setup lang="ts"></script>
+<!-- Start of Selection -->
+<script setup lang="ts">
+import { onMounted } from "vue";
+import { fetchCurrentUser } from "./composables/useAuth";
+
+onMounted(async () => {
+  try {
+    await fetchCurrentUser();
+  } catch (error) {
+    console.error("Error retrieving the current user:", error);
+  }
+});
+</script>
 
 <template>
   <router-view />
