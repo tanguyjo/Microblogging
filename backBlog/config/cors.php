d1fe2a04896a0636e967d7d7b69269e5c7b4ba0a
Theo <theobutz.03@gmail.com>
2025-04-15 12:10:38 +0200

    debug date

--- backBlog/config/cors.php
+++ backBlog/config/cors.php
@@ -19,24 +19,3 @@ return [
 
 ];
 
-<?php
-
-return [
-
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
-    'allowed_methods' => ['*'],
-
-    'allowed_origins' => ['http://localhost:5173'],
-
-    'allowed_origins_patterns' => [],
-
-    'allowed_headers' => ['*'],
-
-    'exposed_headers' => [],
-
-    'max_age' => 0,
-
-    'supports_credentials' => true,
-
-];
